8be8664d17634dd498dab8f6737a255119b3190a
taksho <tky.shj@gmail.com>
2023-01-10 07:13:28 +0000

    Day 17 Secrets

---
+++ secret_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('st.secrets')
+
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
